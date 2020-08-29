cff49ca5956d25fd1bfb1bbf1b5f1135dc6179f9
Vincent <audrbs1541@gmail.com>
2020-08-29 21:05:29 +0900

    practice pygame by Pong game tutorial

---
+++ Python/Pong_1.py
@@ -0,0 +1,54 @@
+import pygame
+from paddle import Paddle
+pygame.init()
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,50,50)
+GREEN = (50,255,50)
+BLUE = (50,50,255)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("PONG GAME")
+
+paddleA = Paddle(RED, 10, 100)
+paddleA.rect.x = 20
+paddleA.rect.y = 200
+
+paddleB = Paddle(BLUE, 10, 100)
+paddleB.rect.x = 670
+paddleB.rect.y = 200
+
+
+all_sprites_list = pygame.sprite.Group()
+
+all_sprites_list.add(paddleA)
+all_sprites_list.add(paddleB)
+
+
+# The loop wil carry on until the user exit the game
+carryOn = True
+
+clock = pygame.time.Clock()
+
+while carryOn:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn = False
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                carryOn = False
+      
+    all_sprites_list.update()
+
+    screen.fill(BLACK)
+    pygame.draw.line(screen, GREEN, [349, 0], [349,500],5)
+    all_sprites_list.draw(screen)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
