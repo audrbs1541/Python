5f7c6c6553a55cddec473b9042fa3ad6c3037806
Vincent <audrbs1541@gmail.com>
2020-08-30 17:43:39 +0900

    add a bouncing ball

--- Python/Pong_1.py
+++ Python/Pong_1.py
@@ -1,8 +1,7 @@
-import sys
 import pygame
-from pygame.locals import QUIT
 
 from paddle import Paddle
+from ball import Ball
 
 pygame.init()
 
@@ -24,12 +23,15 @@ paddleB = Paddle(BLUE, 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
+ballA = Ball(GREEN, 10, 10)
+ballA.rect.x = 345
+ballA.rect.y = 195
 
 all_sprites_list = pygame.sprite.Group()
 
 all_sprites_list.add(paddleA)
 all_sprites_list.add(paddleB)
-
+all_sprites_list.add(ballA)
 
 # The loop wil carry on until the user exit the game
 carryOn = True
@@ -66,8 +68,20 @@ while carryOn:
     
     all_sprites_list.update()
 
+    if ballA.rect.x>=690:
+        ballA.velocity[0] = -ballA.velocity[0]
+    if ballA.rect.x<=0:
+        ballA.velocity[0] = -ballA.velocity[0]
+    if ballA.rect.y>490:
+        ballA.velocity[1] = -ballA.velocity[1]
+    if ballA.rect.y<0:
+        ballA.velocity[1] = -ballA.velocity[1] 
+
+    if pygame.sprite.collide_mask(ballA, paddleA) or pygame.sprite.collide_mask(ballA, paddleB):
+      ballA.bounce()
+
     screen.fill(BLACK)
-    pygame.draw.line(screen, GREEN, [349, 0], [349,500],5)
+    pygame.draw.line(screen, WHITE, [349, 0], [349,500],5)
     all_sprites_list.draw(screen)
 
     pygame.display.flip()
--- Python/paddle.py
+++ Python/paddle.py
@@ -1,4 +1,5 @@
 import pygame
+BLACK = (0,0,0)
 WHITE = (255,255,255)
 
 class Paddle(pygame.sprite.Sprite):
@@ -8,8 +9,8 @@ class Paddle(pygame.sprite.Sprite):
         super().__init__()
 
         self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
 
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
